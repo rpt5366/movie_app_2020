084485571f33ae52db9c8fc3597190b14aed2d0c
King <rpt5366@naver.com>
2022-07-24 15:01:53 +0900

    구조분해 이용하고, 여러 컴포넌트 사용하여 props 출력

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react'
 
-function Food(props){
-  console.log(props);
-  return <h1>I like potato</h1>
+function Food({fav}){
+  return <h1>I like { fav}</h1>
 }
 
 function Potato(){
@@ -13,7 +12,10 @@ function App() {
   return (
   <div>
     <h1>Hello h1</h1>
-    <Food fav="Kimchi" something={true} papapapa={['hello', 1, 2, 3, 4, true]}/>
+    <Food fav="Kimchi"/>
+    <Food fav="ramen" />
+    <Food fav="samgiopsal" />
+    <Food fav="chukumi" />
   </div>
   );
 }
