ddbec0958cf6144d57edbd66f6ce9810188e687f
King <rpt5366@naver.com>
2022-07-24 15:31:54 +0900

    map 함수 및 배열을 사용하여 다수의 컴포넌트 출력

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import React from 'react'
 
-function Food({fav}){
-  return <h1>I like { fav}</h1>
+function Food({name}){
+  return <h1>I like {name}</h1>
 }
 
-function Potato(){
-  return <h1>I like Potato</h1>;
-}
+const foodILike = [
+  {
+    name: "Kimchi",
+    image: 'https://health.chosun.com/site/data/img_dir/2021/09/01/2021090100998_0.jpg'
+  },
+  {
+    name: "Samgyeopsal",
+    image: 'https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/27/117f5b49-1d09-4550-8ab7-87c0d82614de.jpg'
+  },
+  {
+    name: 'Bibimbap',
+    image: 'https://health.chosun.com/site/data/img_dir/2021/01/27/2021012702508_0.jpg'
+  },
+  {
+    name: 'Doncasu',
+    image: 'http://th3.tmon.kr/thumbs/image/6ea/082/d57/2906e06a0_700x700_95_FIT.jpg'
+  },
+  {
+    name: 'Kimbap',
+    image: 'https://homecuisine.co.kr/files/attach/images/140/001/083/558d170258752df2dd76bef00861497f.JPG'
+  },
+];
 
 function App() {
   return (
   <div>
-    <h1>Hello h1</h1>
-    <Food fav="Kimchi"/>
-    <Food fav="ramen" />
-    <Food fav="samgiopsal" />
-    <Food fav="chukumi" />
+    {foodILike.map(dish => (<Food name={dish.name} />))}
   </div>
   );
 }
